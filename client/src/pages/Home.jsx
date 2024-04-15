f2c7f1c21638f5c3c09061f15dc28fd2969c5a11
Fatahillah <Fatahillah@yandex.com>
2024-04-15 13:18:04 +0700

    sidebar feature added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Box } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const Home = () => {
-  return <div>Home</div>;
+
+  const createBoard = () => {
+
+  }
+  
+  return (
+    <Box sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
+    }}>
+      <LoadingButton 
+      variant = 'outlined'
+      color='success'
+      onClick={createBoard}
+      >
+        Click here to create your first board
+      </LoadingButton>
+    </Box>
+  )
 };
 
 export default Home;
